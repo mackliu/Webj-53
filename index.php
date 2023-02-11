96e0863ecbd6a5f4d36304fb3d3df9fbf21439e9
mackliu <macklun@gmail.comm>
2023-02-11 17:09:19 +0800

    修改目錄與課程進度

--- index.php
+++ index.php
@@ -64,6 +64,10 @@
         <td>2023-02-11</td>
         <td><a href="ch5.php">PHP連線資料庫</a></td>
     </tr>
+    <tr>
+        <td>2023-02-11</td>
+        <td><a href="ch6.php">訂票功能與數字驗證</a></td>
+    </tr>
 </table>
 
 
